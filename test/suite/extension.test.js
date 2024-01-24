686bab949f23bd40f5e622c4b4fe8e1713b09f1d
Djamila Baroudi <db@blksail.ai>
2024-01-24 23:33:37 +0100

    adds command excution unit tests

--- test/suite/extension.test.js
+++ test/suite/extension.test.js
@@ -3,13 +3,29 @@ const assert = require('assert');
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 const vscode = require('vscode');
-// const myExtension = require('../extension');
+const bruinExtension = require('../../extension');
 
-suite('Extension Test Suite', () => {
+suite('Bruin Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+	test('should return an object with stdout property', async () => {
+	  const outputCommand = await bruinExtension.commandExcution('bruin --help');
+	  assert.ok(outputCommand.hasOwnProperty('stdout'));
 	});
+	
+	test('should return an object with stdout containing the output of the bruin --help command', async () => {
+	  const expectedOutput = `bruin - The CLI used for managing Bruin-powered data pipelines`;
+	  const outputCommand = await bruinExtension.commandExcution('bruin --help');
+	assert.ok(outputCommand.stdout.includes(expectedOutput));
+	});
+
+	test('should return an error message if the command fails', async () => {
+		const errorCommand = await bruinExtension.commandExcution('bruin render null');
+	  assert.ok(errorCommand.stderr.includes("Command failed"));
+	});
+
+	test('should return an error message if the command does not exist', async () => {
+		const errorCommand = await bruinExtension.commandExcution('command-not-found');
+		assert.ok(errorCommand.stderr.includes("command not found"));
+	})
 });
