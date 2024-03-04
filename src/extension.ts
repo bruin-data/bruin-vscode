5fcd538ab891dd53c868a5fe62721005ea15ed4c
Djamila Baroudi <djamila.baroudi@getbruin.com>
2024-03-04 12:17:20 +0100

    Migrate to TypeScript

--- src/extension.ts
+++ src/extension.ts
@@ -1,21 +1,12 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
+import * as vscode from 'vscode';
+
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
-
-/**
- * @param {vscode.ExtensionContext} context
- */
-function activate(context) {
-	//
+export function activate(context: vscode.ExtensionContext) {
 }
 
 // This method is called when your extension is deactivated
-function deactivate() {}
-
-module.exports = {
-	activate,
-	deactivate
-}
+export function deactivate() {}
