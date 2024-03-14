986f5fe5023f7066249f23b88693f49c1a47e06f
Djamila Baroudi <baroudi.d7@gmail.com>
2024-03-14 15:38:36 +0100

    add folding settings to the extension

--- src/extension.ts
+++ src/extension.ts
@@ -1,32 +1,59 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
-import { bruinFoldingRangeProvider } from "./providers/bruinFoldingRangeProvider";
-import {
-  isBruinBinaryAvailable,
-} from "./utils/bruinUtils";
 
-import { BruinMainPanel } from './panels/BruinMainPanel';
+import { bruinFoldingRangeProvider } from "./providers/bruinFoldingRangeProvider";
+
+import { isBruinBinaryAvailable } from "./utils/bruinUtils";
+
+import { BruinMainPanel } from "./panels/BruinMainPanel";
 
 export function activate(context: vscode.ExtensionContext) {
-    if (!isBruinBinaryAvailable()) {
-        vscode.window.showErrorMessage("Bruin is not installed");
-        return;
+  if (!isBruinBinaryAvailable()) {
+    vscode.window.showErrorMessage("Bruin is not installed");
+
+    return;
+  }
+
+  const foldingDisposable = vscode.languages.registerFoldingRangeProvider(
+    ["python", "sql"],
+
+    {
+      provideFoldingRanges: bruinFoldingRangeProvider,
     }
-    const foldingDisposable = vscode.languages.registerFoldingRangeProvider(
-      ["python", "sql"],
-      {
-        provideFoldingRanges: bruinFoldingRangeProvider,
-      }
-    );
-    context.subscriptions.push(
-        vscode.commands.registerCommand('bruin.renderSQL', () => {
-            BruinMainPanel.createOrShow(context.extensionUri, context);
-        }),
-        foldingDisposable
-    );
+  );
+
+  applyFoldingStateBasedOnConfiguration();
+
+  // Listen for configuration changes to apply folding state
+
+  vscode.workspace.onDidChangeConfiguration((e) => {
+    if (e.affectsConfiguration("bruin.defaultFoldingState")) {
+      applyFoldingStateBasedOnConfiguration();
+    }
+  });
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand("bruin.renderSQL", () => {
+      BruinMainPanel.createOrShow(context.extensionUri, context);
+    }),
+
+    foldingDisposable,
+
+    vscode.window.onDidChangeActiveTextEditor(() => {
+      applyFoldingStateBasedOnConfiguration();
+    })
+  );
 }
 
-
 // This method is called when your extension is deactivated
+
 export function deactivate() {}
+
+function applyFoldingStateBasedOnConfiguration() {
+  const config = vscode.workspace.getConfiguration();
+
+  const defaultFoldingState = config.get("bruin.defaultFoldingState", "folded");
+
+  if (defaultFoldingState === "folded" && vscode.window.activeTextEditor) {
+    vscode.commands.executeCommand("editor.foldAllMarkerRegions");
+  }
+}
