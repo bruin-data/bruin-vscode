b2c9d9b6f4101a60c865047b1eba2082855f410f
Djamila Baroudi <baroudi.d7@gmail.com>
2024-04-25 10:02:45 +0200

    implement a tabbed layout in the webview UI

---
+++ webview-ui/src/components/AssetLineage.vue
@@ -0,0 +1,14 @@
+<template>
+
+    <div class="flex flex-col p-4 space-y-4">
+      <div class="flex flex-col space-y-3">
+        <div class="flex flex-wrap gap-y-4">
+            <p>
+                This is supposed to be the asset lineage component
+            </p>
+        </div>
+    </div>
+    </div>
+  </template>
+  
+  
