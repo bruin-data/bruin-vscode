198ca1630e6951030ca4e11dcd8c88766a82eab0
Djamila Baroudi <baroudi.d7@gmail.com>
2024-05-06 09:53:00 +0200

    fix typing problem

--- webview-ui/src/types/index.ts
+++ webview-ui/src/types/index.ts
@@ -2,3 +2,20 @@ export type CheckboxItems = {
     name: string;
     checked: boolean;
 };
+
+
+ interface Issue {
+    asset: string;
+    description: string;
+    context: string[];
+  }
+  
+  interface IssuesMap {
+    [test: string]: Issue[];
+  }
+  
+  export interface ParsedErrorMessage {
+    pipeline: string;
+    issues: IssuesMap;
+  }
+  
