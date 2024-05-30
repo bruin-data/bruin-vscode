4ff41745d15b4ed61866f45a3270950fd9eb0255
Djamila Baroudi <baroudi.d7@gmail.com>
2024-05-30 19:18:07 +0200

    experiment with asset details styling to explore potential improvements

--- webview-ui/src/components/AssetDetails.vue
+++ webview-ui/src/components/AssetDetails.vue
@@ -1,37 +1,56 @@
 <template>
   <div
     v-if="props !== null"
-    class="flex h-full w-full flex-col overflow-scroll overflow-x-auto pt-6 shadow-xl text-gray-500"
+    class="flex flex-col h-1/4 overflow-auto shadow-xl text-gray-500 bg-editor-bg max-h-72"
   >
-    <div class="px-4 sm:px-6 border-b pb-4">
-      <h2 class="text-lg font-semibold font-mono leading-6 text-gray-300">
-        {{ name }}
-      </h2>
+    <div class="mt-8 flow-root">
+      <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+          <table class="min-w-full divide-y divide-editor-border">
+            <tbody class="divide-y divide-editor-border bg-editor-bg">
+              <tr>
+                <td class="whitespace-nowrap py-5 pl-4 pr-3 text-sm sm:pl-0">
+                  <div class="flex items-center">
+                    <div class="ml-4">
+                      <div class="font-md text-editor-fg text-lg">{{ name }}</div>
+                    </div>
+                  </div>
+                </td>
+                <td class="whitespace-nowrap px-3 py-5 text-sm text-editor-fg">
+                  <DescriptionItem title="Type" :value="type" :className="badgeClass.badgeStyle" />
+                </td>
+                <td class="whitespace-nowrap px-3 py-5 text-sm text-editor-fg">
+                  <DescriptionItem
+                    title="Schedule"
+                    :value="pipeline.schedule"
+                    :className="badgeClass.grayBadge"
+                  />
+                </td>
+                <td class="whitespace-nowrap px-3 py-5 text-sm text-editor-fg">
+                  <DescriptionItem
+                    title="Owner"
+                    value="Unknown"
+                    className="font-semibold text-gray-400"
+                  />
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
-    <div class="relative overflow-scroll py-6 flex-1 px-4 sm:px-6">
-      <dl class="divide-y divide-gray-600">
-        <DescriptionItem title="Asset" :value="name" />
-        <DescriptionItem title="Type" :value="type" :className="badgeClass.badgeStyle" />
-        <DescriptionItem
-          title="Schedule"
-          :value="pipeline.schedule"
-          :className="badgeClass.grayBadge"
-        />
-        <DescriptionItem title="Owner" value="Unknown" className="font-semibold text-gray-400" />
-      </dl>
-    </div>
-    <div class="border-b flex flex-col gap-4 py-6 flex-1 px-4 sm:px-6">
+    <div class="border-b py-6 px-4 sm:px-6 bg-editor-bg">
       <h3 class="text-lg leading-6 font-medium text-gray-100 mb-2">Description</h3>
-      <div class="max-w-3xl lg:mt-0 xl:col-end-1 xl:row-start-1">
+      <div class="max-w-3xl">
         <p
-          class="max-w-full text-sm leading-8 text-editor-fg opacity-65 text-justify indent-8 prose"
+          class="max-h-full overflow-auto text-sm leading-8 text-editor-fg opacity-65 text-justify indent-8 prose"
           v-html="markdownDescription"
         ></p>
       </div>
     </div>
   </div>
   <div class="flex" v-else>
-    <MessageAlert message="This file is not a Bruin Asset or has No data to dipslay" />
+    <MessageAlert message="This file is not a Bruin Asset or has No data to display" />
   </div>
 </template>
 
--- webview-ui/src/components/ui/description-item/DescriptionItem.vue
+++ webview-ui/src/components/ui/description-item/DescriptionItem.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="px-4 py-2 items-center mt-2 mb-2 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-0">
     <dt>
-      <span class="mt-1 text-sm text-gray-300 leading-6 sm:col-span-3 sm:mt-0 font-mono">
+      <span class="mt-1 text-sm text-editor-fg opacity-50 leading-6 sm:col-span-3 sm:mt-0 font-mono">
         {{ props.title }}
       </span>
     </dt>
-    <dd class="mt-1 text-sm text-gray-300 leading-6 sm:col-span-3 sm:mt-0 font-mono">
+    <dd class="mt-1 text-sm text-editor-fg leading-6 sm:col-span-3 sm:mt-0 font-mono">
       <span
         :class="props.className">
         {{ props.value }}
@@ -18,7 +18,7 @@
 import { defineProps } from "vue";
 
 const props = defineProps<{
-  title: string;
+  title?: string;
   value: string;
   className?: string;
 }>();
