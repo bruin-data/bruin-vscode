5c24cfb48225ba19440960859e4b9418809eb1c6
Djamila Baroudi <baroudi.d7@gmail.com>
2024-08-20 13:51:02 +0200

    move connection to settings tab

---
+++ webview-ui/src/components/bruin-settings/BruinSettings.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex flex-col justify-between">
+    <div>
+      <BruinCLI />
+    </div>
+    <div class="mt-2">
+      <ConnectionsList />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import BruinCLI from "@/components/bruin-settings/BruinCLI.vue";
+import ConnectionsList from "@/components/connections/ConnectionList.vue";
+</script>
