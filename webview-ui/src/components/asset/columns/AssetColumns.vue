68bdc4cfe87fdcc70e73e001db70b23d8be3ce58
Djamila Baroudi <baroudi.d7@gmail.com>
2024-08-29 13:41:19 +0200

    add toggle switch for 'blocking' property

--- webview-ui/src/components/asset/columns/AssetColumns.vue
+++ webview-ui/src/components/asset/columns/AssetColumns.vue
@@ -16,34 +16,45 @@
       class="flex flex-col bg-editorWidget-bg mb-2 rounded-b-lg shadow-sm"
     >
       <!-- Column Details -->
-      <div class="flex p-4 border-b items-center">
-        <div class="w-1/4 font-semibold text-editor-fg">{{ column.name }}</div>
+      <div class="flex p-4 border-b border-commandCenter-border items-center">
+        <div class="w-1/4 font-semibold text-editor-fg text-md">{{ column.name }}</div>
         <div class="w-1/4">
           <vscode-tag>{{ column.type.toUpperCase() }}</vscode-tag>
         </div>
-        <div class="w-1/2 text-editor-fg text-sm">{{ column.description }}</div>
+        <div class="w-1/2 text-editor-fg opacity-50 text-sm">{{ column.description }}</div>
       </div>
 
       <!-- Checks Section -->
       <div class="p-4">
         <div class="flex justify-between mb-2">
-          <h3 class="text-md font-semibold text-editor-fg">Checks</h3>
+          <h3 class="text-lg font-semibold text-editor-fg">Checks</h3>
           <!-- Blocking switch -->
           <div class="flex items-center space-x-1">
-            <span class="text-sm text-editor-fg"> Blocking </span>
-            <vscode-checkbox checked></vscode-checkbox>
+            <label class="inline-flex items-center cursor-pointer">
+              <input type="checkbox" class="sr-only peer" checked disabled />
+
+              <div
+                class="relative w-9 h-5 bg-input-background rounded-full peer-checked:bg-editor-button-bg peer-checked:after:translate-x-[14px] after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-editor-fg after:border after:rounded-full after:h-4 after:w-4 after:transition-all peer-disabled:opacity-60 peer-disabled:cursor-not-allowed"
+              ></div>
+
+              <span
+                class="ml-3 text-sm font-medium text-[var(--vscode-editor-foreground)] opacity-50"
+              >
+                Blocking
+              </span>
+            </label>
           </div>
         </div>
         <div class="flex flex-wrap gap-2">
-          <vscode-badge v-for="check in getActiveChecks(column)" :key="check" >
+          <vscode-badge v-for="check in getActiveChecks(column)" :key="check" class="custom-badge">
             {{ check }}
           </vscode-badge>
         </div>
-        <div v-if="column.checks.acceptedValuesEnabled" class="mt-2">
+        <!-- <div v-if="column.checks.acceptedValuesEnabled" class="mt-2">
           <span class="text-sm text-editor-fg">
             Accepted values: {{ column.checks.accepted_values.join(", ") }}
           </span>
-        </div>
+        </div> -->
         <div v-if="column.checks.patternEnabled && column.checks.pattern" class="mt-2">
           <span class="text-sm text-editor-fg"> Pattern: {{ column.checks.pattern }} </span>
         </div>
@@ -79,3 +90,8 @@ const getActiveChecks = (column) => {
   return activeChecks;
 };
 </script>
+<style scoped>
+vscode-badge::part(control) {
+  background-color: var(--vscode-button-background);
+}
+</style>
