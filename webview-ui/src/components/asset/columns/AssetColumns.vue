3eff7704325b1f95f20e13d17cb4a11bc95a24db
Djamila Baroudi <baroudi.d7@gmail.com>
2024-09-02 17:32:41 +0200

    fix the overflow when resizing

--- webview-ui/src/components/asset/columns/AssetColumns.vue
+++ webview-ui/src/components/asset/columns/AssetColumns.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="flex flex-col py-4 sm:py-1 bg-editorWidget-bg">
     <!-- Header Row -->
-    <div class="flex p-2 sm:p-2 font-semibold text-editor-fg text-md opacity-65 border-b-2 border-editor-fg">
+    <div
+      class="flex p-2 sm:p-2 font-semibold text-editor-fg text-md opacity-65 border-b-2 border-editor-fg"
+    >
       <div class="flex-1 min-w-0 px-2 text-left">Name</div>
       <div class="flex-1 min-w-0 px-2 text-left">Type</div>
-      <div class="flex-2 min-w-0 px-2 text-left">Description</div> 
+      <div class="flex-[2] min-w-0 px-2 text-left">Description</div>
       <div class="flex-1 min-w-0 px-2 text-left">Checks</div>
     </div>
 
     <!-- Column Rows -->
-    <div class="flex-1 min-h-0 overflow-auto">
+    <div class="flex-1 min-h-0">
       <div
         v-if="columns.length"
         v-for="(column, index) in columns"
@@ -21,29 +23,58 @@
           <div class="truncate" :title="column.name">{{ column.name }}</div>
         </div>
         <div class="flex-1 min-w-0 px-2 text-left">
-          <div v-if="column.type" class="flex-1 min-w-0 px-2 text-left text-[0.7rem] opacity-70 truncate">{{ column.type.toUpperCase() }}</div>
-          <div class="flex-1 min-w-0 px-2 text-left text-editor-fg opacity-30 text-xs sm:text-xs" v-else>undefined</div>
+          <div
+            v-if="column.type"
+            class="flex-1 min-w-0 px-2 text-left text-[0.7rem] opacity-70 truncate"
+            :title="column.type.toUpperCase()"
+          >
+            {{ column.type.toUpperCase() }}
+          </div>
+          <div
+            class="flex-1 min-w-0 px-2 text-left text-editor-fg opacity-30 text-xs sm:text-xs"
+            v-else
+          >
+            undefined
+          </div>
         </div>
-        <div v-if="column.description" class="flex-2 min-w-0 px-2 text-left text-commandCenter-fg opacity-70 font-thin">
+        <div
+          v-if="column.description"
+          class="flex-[2] min-w-0 px-2 text-left text-commandCenter-fg opacity-70 font-thin"
+        >
           {{ column.description }}
         </div>
-        <div v-else class="flex-[1.5] min-w-0 px-2 text-left text-commandCenter-fg">No description provided.</div>
+        <div v-else class="flex-[1.5] min-w-0 px-2 text-left text-commandCenter-fg">
+          No description provided.
+        </div>
         <!-- Checks Column -->
         <div class="flex-1 min-w-0 px-2 text-left flex flex-wrap gap-2 whitespace-nowrap">
           <vscode-badge
             v-for="check in getActiveChecks(column)"
             :key="check"
             :class="{ 'relative cursor-pointer': check === 'accepted_values' }"
-            :title="check === 'accepted_values' ? column.checks.accepted_values.join('\n') : ''"
+            :title="check === 'accepted_values' ? 'accepted_values \n' + column.checks.accepted_values.join('\n') : ''"
+            :style="{
+              overflow: 'hidden',
+              textOverflow: 'ellipsis',
+              whiteSpace: 'nowrap',
+            }"
           >
-            {{ check }}
+            <span class="block truncate max-w-[100px]">{{ check }}</span>
           </vscode-badge>
-          <div v-if="column.checks.patternEnabled && column.checks.pattern" class="text-sm sm:text-xs text-editor-fg whitespace-nowrap">
+          <div
+            v-if="column.checks.patternEnabled && column.checks.pattern"
+            class="text-sm sm:text-xs text-editor-fg whitespace-nowrap"
+          >
             Pattern: {{ column.checks.pattern }}
           </div>
         </div>
       </div>
-      <div v-else class="flex p-2 sm:p-2 bg-editorWidget-bg mb-2 text-editor-fg opacity-50 font-light italic">No columns provided.</div>
+      <div
+        v-else
+        class="flex p-2 sm:p-2 bg-editorWidget-bg mb-2 text-editor-fg opacity-50 font-light italic"
+      >
+        No columns provided.
+      </div>
     </div>
   </div>
 </template>
@@ -76,5 +107,4 @@ const getActiveChecks = (column) => {
 };
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
