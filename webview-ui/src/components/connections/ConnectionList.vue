cbab56222e286e8c4c87da08c2dac249cd0030ee
Djamila Baroudi <baroudi.d7@gmail.com>
2024-09-05 13:50:46 +0200

    add a description to the connection section

--- webview-ui/src/components/connections/ConnectionList.vue
+++ webview-ui/src/components/connections/ConnectionList.vue
@@ -84,35 +84,77 @@ vscode-button::part(control) {
 }
 </style> -->
 
-
-
 <template>
   <div class="max-w-7xl mx-auto p-4">
-    <div class="sm:flex sm:items-center sm:justify-between">
+    <div class="sm:flex sm:items-center sm:justify-between mb-2">
       <div class="flex flex-col items-start space-y-2">
         <h2 class="text-xl font-semibold text-editor-fg">Connections</h2>
-        <div>This is a description for the connection list</div>
+        <div class="mt-2 max-w-xl text-sm text-editor-fg">
+          <p>
+            View your connections across different environments in one place.
+          </p>
+        </div>
       </div>
+      <!--  <vscode-button
+        @click="$emit('new-connection')"
+        class="mt-3 sm:mt-0 rounded-md px-3 py-2 text-sm font-semibold"
+      >
+        New connection
+      </vscode-button> -->
     </div>
-    
+
     <div v-for="(connections, environment) in groupedConnections" :key="environment" class="mt-6">
       <h3 class="text-lg font-medium text-editor-fg mb-2 font-mono">{{ environment }}</h3>
       <div class="overflow-hidden bg-editorWidget-bg sm:rounded-lg">
         <table class="min-w-full divide-y divide-commandCenter-border">
           <thead>
             <tr>
-              <th scope="col" class="w-1/2 px-2 py-2 text-left text-sm font-semibold text-editor-fg opacity-70 ">Name</th>
-              <th scope="col" class="w-1/2 px-2 py-2 text-left text-sm font-semibold text-editor-fg opacity-70">Type</th>
+              <th
+                scope="col"
+                class="w-1/2 px-2 py-2 text-left text-sm font-semibold text-editor-fg opacity-70"
+              >
+                Name
+              </th>
+              <th
+                scope="col"
+                class="w-1/2 px-2 py-2 text-left text-sm font-semibold text-editor-fg opacity-70"
+              >
+                Type
+              </th>
             </tr>
           </thead>
           <tbody>
-            <tr v-for="connection in connections" :key="connection.name" class="hover:bg-editor-hoverBackground">
-              <td class="w-1/2 whitespace-nowrap px-2 py-4 text-sm font-medium text-editor-fg font-mono">
+            <tr
+              v-for="connection in connections"
+              :key="connection.name"
+              class="hover:bg-editor-hoverBackground"
+            >
+              <td
+                class="w-1/2 whitespace-nowrap px-2 py-4 text-sm font-medium text-editor-fg font-mono"
+              >
                 {{ connection.name }}
               </td>
               <td class="w-1/2 whitespace-nowrap px-2 py-4 text-sm text-descriptionFg font-mono">
                 {{ connection.type }}
               </td>
+              <!--  <td
+                class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6"
+              >
+                <button
+                  @click="$emit('edit-connection', connection)"
+                  class="text-descriptionFg hover:text-editor-fg mr-3"
+                  title="Edit"
+                >
+                  <PencilIcon class="h-5 w-5" />
+                </button>
+                <button
+                  @click="$emit('delete-connection', connection)"
+                  class="text-errorForeground hover:text-editorError-foreground"
+                  title="Delete"
+                >
+                  <TrashIcon class="h-5 w-5" />
+                </button>
+              </td> -->
             </tr>
           </tbody>
         </table>
@@ -124,6 +166,7 @@ vscode-button::part(control) {
 <script setup>
 import { useConnectionsStore } from "@/store/connections";
 import { computed } from "vue";
+import { TrashIcon, PencilIcon } from "@heroicons/vue/24/outline";
 
 const connectionsStore = useConnectionsStore();
 const connections = computed(() => connectionsStore.connections);
@@ -136,7 +179,7 @@ const groupedConnections = computed(() => {
   }, {});
 });
 
-defineEmits(['new-connection', 'edit-connection', 'delete-connection']);
+defineEmits(["new-connection", "edit-connection", "delete-connection"]);
 </script>
 
 <style scoped>
@@ -144,7 +187,7 @@ vscode-button::part(control) {
   border: none;
   outline: none;
 }
-.divide-y-opacity{
+.divide-y-opacity {
   border: 0.5;
 }
 </style>
