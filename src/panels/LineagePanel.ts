a88015dfc1b483b7df6c50ca18584116b770aa84
Djamila Baroudi <baroudi.d7@gmail.com>
2024-10-23 19:23:23 +0200

    update Content-Security-Policy in BruinPanel and LineagePanel

--- src/panels/LineagePanel.ts
+++ src/panels/LineagePanel.ts
@@ -128,7 +128,13 @@ public resolveWebviewView(
       <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src ${webview.cspSource} https:; script-src 'nonce-${nonce}'; style-src ${webview.cspSource};">
+        <meta http-equiv="Content-Security-Policy" content="
+          default-src 'none';
+          img-src ${webview.cspSource} https:;
+          script-src 'nonce-${nonce}' ${webview.cspSource};
+          style-src ${webview.cspSource} 'unsafe-inline';
+          font-src ${webview.cspSource};
+        ">        
         <link rel="stylesheet" href="${stylesUri}">
         <title>Bruin Lineage</title>
       </head>
