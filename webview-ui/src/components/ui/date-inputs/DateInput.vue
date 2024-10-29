c315a206bc6f8e235df31f4cb0f5283e56737518
Djamila Baroudi <baroudi.d7@gmail.com>
2024-10-29 10:29:56 +0100

    refactor date input component styles

--- webview-ui/src/components/ui/date-inputs/DateInput.vue
+++ webview-ui/src/components/ui/date-inputs/DateInput.vue
@@ -1,21 +1,19 @@
 <template>
-  <div class="flex flex-col w-40">
+  <div class="flex flex-col w-32">
     <label class="text-xs mb-1 font-medium">{{ label }}</label>
     <div class="relative">
       <input
         type="datetime-local"
-        class="w-full text-xs p-1.5 border border-commandCenter-border rounded-sm bg-input-background focus:border-input-border"
+        class="w-full text-2xs px-1 py-0.5 border border-commandCenter-border rounded-sm bg-input-background focus:border-input-border"
         :value="modelValue"
         @input="updateValue($event)"
       />
-      <CalendarIcon class="absolute right-1 top-1/2 -translate-y-1/2 w-4 h-4 pointer-events-none text-input-foreground" />
-    </div>
+   </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { defineProps, defineEmits } from "vue";
-import { CalendarIcon } from "@heroicons/vue/20/solid";
 
 defineProps({
   label: String,
