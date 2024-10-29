fa149a5326c3be635d2f52c59e1d51784c21524b
Djamila Baroudi <baroudi.d7@gmail.com>
2024-10-29 15:16:55 +0100

    Refactor CheckboxGroup component to remove expand/collapse functionality

--- webview-ui/src/components/ui/checkbox-group/CheckboxGroup.vue
+++ webview-ui/src/components/ui/checkbox-group/CheckboxGroup.vue
@@ -1,17 +1,5 @@
 <template>
   <div class="flex flex-col w-full">
-    <button
-      @click="toggleExpanded"
-      class="flex items-center gap-2 mt-2 text-xs text-textLink-activeForeground hover:text-editorLink-activeForeground transition-colors"
-    >
-      <span>{{ label }}</span>
-      <component
-        :is="expanded ? ChevronUpIcon : ChevronDownIcon"
-        class="h-4 w-4"
-        aria-hidden="true"
-      />
-    </button>
-
     <div v-if="expanded" class="flex flex-wrap rounded-none mt-2">
       <vscode-checkbox
         v-for="(item, index) in checkboxItems"
@@ -28,18 +16,13 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import type { CheckboxItems } from "@/types";
-import { ChevronDownIcon, ChevronUpIcon } from "@heroicons/vue/20/solid";
 
 const props = defineProps<{
   checkboxItems: CheckboxItems[];
   label: string;
 }>();
 
-const expanded = ref(false);
-
-function toggleExpanded() {
-  expanded.value = !expanded.value;
-}
+const expanded = ref(true);
 
 function handleCheckboxChange(item: any, event: any) {
   item.checked = event.target.checked;
