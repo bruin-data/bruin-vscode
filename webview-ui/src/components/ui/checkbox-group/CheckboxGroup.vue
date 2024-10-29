077c9299fe8ec90e49f1930a33eb992545f2a5c4
Djamila Baroudi <baroudi.d7@gmail.com>
2024-10-29 10:29:45 +0100

    refactor CheckboxGroup component to add expand/collapse functionality

--- webview-ui/src/components/ui/checkbox-group/CheckboxGroup.vue
+++ webview-ui/src/components/ui/checkbox-group/CheckboxGroup.vue
@@ -1,21 +1,47 @@
 <template>
-      <div class="flex flex-wrap rounded-none">
-        <vscode-checkbox v-for="(item, index) in props.checkboxItems"
-          :key="index" 
-          @change="handleCheckboxChange(item, $event)"
-          :checked="item.checked">
-          {{ item.name }}   
-        </vscode-checkbox>
+  <div class="flex flex-col w-full">
+    <button
+      @click="toggleExpanded"
+      class="flex items-center gap-2 mt-2 text-xs text-textLink-activeForeground hover:text-editorLink-activeForeground transition-colors"
+    >
+      <span>{{ label }}</span>
+      <component
+        :is="expanded ? ChevronUpIcon : ChevronDownIcon"
+        class="h-4 w-4"
+        aria-hidden="true"
+      />
+    </button>
 
+    <div v-if="expanded" class="flex flex-wrap rounded-none mt-2">
+      <vscode-checkbox
+        v-for="(item, index) in checkboxItems"
+        :key="index"
+        @change="handleCheckboxChange(item, $event)"
+        :checked="item.checked"
+      >
+        {{ item.name }}
+      </vscode-checkbox>
     </div>
+  </div>
 </template>
+
 <script setup lang="ts">
+import { ref } from "vue";
 import type { CheckboxItems } from "@/types";
+import { ChevronDownIcon, ChevronUpIcon } from "@heroicons/vue/20/solid";
 
 const props = defineProps<{
-    checkboxItems: CheckboxItems[];
+  checkboxItems: CheckboxItems[];
+  label: string;
 }>();
+
+const expanded = ref(false);
+
+function toggleExpanded() {
+  expanded.value = !expanded.value;
+}
+
 function handleCheckboxChange(item: any, event: any) {
-    item.checked = event.target.checked;
+  item.checked = event.target.checked;
 }
 </script>
