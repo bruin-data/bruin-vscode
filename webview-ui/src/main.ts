fd1d4e94f5960ddb00452233952edbcc266be235
Djamila Baroudi <baroudi.d7@gmail.com>
2024-12-11 13:51:31 +0100

    remove RudderStack from the main.ts

--- webview-ui/src/main.ts
+++ webview-ui/src/main.ts
@@ -6,22 +6,12 @@ import sql from "highlight.js/lib/languages/sql";
 import hljsVuePlugin from "@highlightjs/vue-plugin";
 import { allComponents, provideVSCodeDesignSystem } from "@vscode/webview-ui-toolkit";
 import { createPinia } from "pinia";
-import RudderStackService from "./services/RudderStackService";
-
-// RudderStack Configuration
-
 
 const app = createApp(App);
-// Create a plugin to make RudderStack available across the app
-const RudderStackPlugin = {
-  install(app) {
-    app.config.globalProperties.$rudderStack = RudderStackService.getInstance();
-  },
-};
+
 provideVSCodeDesignSystem().register(allComponents);
 
 hljs.registerLanguage("sql", sql);
 app.use(hljsVuePlugin);
 app.use(createPinia());
-app.use(RudderStackPlugin);
 app.mount("#app");
