88de364a43e887807149d60cf7c7e9c317457f9f
Djamila Baroudi <baroudi.d7@gmail.com>
2024-12-18 17:41:55 +0100

    remove unused imports and update command execution for platform-specific paths

--- src/ui-test/commands.test.ts
+++ src/ui-test/commands.test.ts
@@ -3,7 +3,6 @@ import { Workbench } from 'vscode-extension-tester';
 import { TerminalView } from 'vscode-extension-tester';
 import 'mocha';
 import { describe, it } from 'mocha';
-import { getDefaultBruinExecutablePath } from '../extension/configuration';
 
 const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
@@ -11,10 +10,15 @@ describe('Sample Command palette tests', function () {
     it('Testing install Bruin CLI command', async () => {
 		await new Workbench().executeCommand('Install Bruin CLI');
         await sleep(5000);
+        const bruinPathWindows = "~/.local/bin/bruin.exe";
         const terminalView = await new TerminalView();
         await terminalView.selectChannel("Bruin Terminal");
-        const executablePath = getDefaultBruinExecutablePath();
-        await terminalView.executeCommand(`${executablePath} --version`);
+        if (process.platform === 'win32') {
+            await terminalView.executeCommand(`${bruinPathWindows} --version`);
+        } else {
+            await terminalView.executeCommand(`bruin --version`);
+        }
+        //await terminalView.executeCommand("bruin --version");
         const terminalOutput = await terminalView.getText();
         console.log(terminalOutput);
         const versionAvailble = terminalOutput.includes('Current: ') && terminalOutput.includes('Latest: ');
