d5d3df26453d93a92406afa0ec4feff2b23b3cc0
Djamila Baroudi <baroudi.d7@gmail.com>
2024-12-18 15:04:09 +0100

    restore @types/mocha dependency in package.json and update test suite to use describe and it functions

--- src/test/ui/commands.test.ts
+++ src/test/ui/commands.test.ts
@@ -1,13 +1,12 @@
 import * as assert from 'assert';
 import { Workbench } from 'vscode-extension-tester';
-import { suite, test } from 'mocha';
 import { TerminalView } from 'vscode-extension-tester';
 const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
-suite('Sample Command palette tests', function (this: Mocha.Suite) {
+describe('Sample Command palette tests', function (this: Mocha.Suite) {
     this.timeout(150000); 
 
-    test('Testing install Bruin CLI command', async () => {
+    it('Testing install Bruin CLI command', async () => {
 		await new Workbench().executeCommand('Install Bruin CLI');
         await sleep(5000);
         const terminalView = await new TerminalView();
