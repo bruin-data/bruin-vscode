e07f868ffe3f12bf64fcf5cc5cd6ea85e4a03b80
Djamila Baroudi <baroudi.d7@gmail.com>
2024-12-18 16:16:15 +0100

    update test workflow and configuration for improved test execution

--- src/test/ui/commands.test.ts
+++ src/test/ui/commands.test.ts
@@ -2,14 +2,11 @@ import * as assert from 'assert';
 import { Workbench } from 'vscode-extension-tester';
 import { TerminalView } from 'vscode-extension-tester';
 import 'mocha';
-import { describe } from 'mocha';
-import { it } from 'mocha';
+import { describe, it } from 'mocha';
 
 const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
-describe('Sample Command palette tests', function (this: Mocha.Suite) {
-    this.timeout(150000); 
-
+describe('Sample Command palette tests', function () {
     it('Testing install Bruin CLI command', async () => {
 		await new Workbench().executeCommand('Install Bruin CLI');
         await sleep(5000);
