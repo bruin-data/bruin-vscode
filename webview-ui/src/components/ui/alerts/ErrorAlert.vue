e51ba7b176404a4c1a9faa0c977357dab3995c4c
Djamila Baroudi <baroudi.d7@gmail.com>
2025-02-04 16:46:06 +0100

    format

--- webview-ui/src/components/ui/alerts/ErrorAlert.vue
+++ webview-ui/src/components/ui/alerts/ErrorAlert.vue
@@ -3,7 +3,7 @@
   <div
     v-if="processedErrors.length"
     class="rounded-md bg-red-50 p-4 my-4 overflow-hidden transition-all duration-300"
-    :class="{ 'h-16':!isExpanded, 'max-h-64': isExpanded }"
+    :class="{ 'h-16': !isExpanded, 'max-h-64': isExpanded }"
   >
     <!-- Header section with error icon, title, and control buttons -->
     <div class="flex justify-between items-center">
@@ -14,7 +14,11 @@
       <div class="flex items-center">
         <!-- Toggle button for expanding/collapsing the error details -->
         <button @click="isExpanded = !isExpanded" class="text-red-500 mr-2">
-          <component :is="isExpanded ? ChevronUpIcon : ChevronDownIcon" class="h-5 w-5" aria-hidden="true" />
+          <component
+            :is="isExpanded ? ChevronUpIcon : ChevronDownIcon"
+            class="h-5 w-5"
+            aria-hidden="true"
+          />
         </button>
         <!-- Button to close the error alert -->
         <button @click="$emit('errorClose')" class="text-red-700">
@@ -28,17 +32,30 @@
         <!-- Single Asset Validation Display -->
         <div v-if="isSingleAsset" class="mt-4">
           <h4 class="text-md font-semibold text-gray-900">Asset: {{ singleAssetName }}</h4>
-          <div v-for="(issue, issueIndex) in errorMessage.issues" :key="issueIndex" class="mb-2 ml-2">
+          <div
+            v-for="(issue, issueIndex) in errorMessage.issues"
+            :key="issueIndex"
+            class="mb-2 ml-2"
+          >
             <p class="text-sm text-red-600">{{ issue.description }}</p>
             <div v-if="issue.context.length" class="flex items-center space-x-1 mt-2 justify-end">
               <!-- Button to toggle the expansion of issue details -->
-              <button @click="toggleIssueExpansion(index, issueIndex)" class="text-xs text-red-700 flex items-center">
+              <button
+                @click="toggleIssueExpansion(index, issueIndex)"
+                class="text-xs text-red-700 flex items-center"
+              >
                 <span class="mr-1">Details</span>
-                <component :is="issue.expanded ? ChevronUpIcon : ChevronDownIcon" class="h-4 w-4" aria-hidden="true" />
+                <component
+                  :is="issue.expanded ? ChevronUpIcon : ChevronDownIcon"
+                  class="h-4 w-4"
+                  aria-hidden="true"
+                />
               </button>
             </div>
             <div v-if="issue.expanded" class="text-sm text-gray-700 ml-1 mt-1 w-full">
-              <pre class="whitespace-pre-wrap bg-red-100 rounded p-2">{{ issue.context.join('\n') }}</pre>
+              <pre class="whitespace-pre-wrap bg-red-100 rounded p-2">{{
+                issue.context.join("\n")
+              }}</pre>
             </div>
           </div>
         </div>
@@ -46,21 +63,36 @@
         <!-- Full Pipeline Validation Display -->
         <div v-else>
           <div @click="toggleExpansion(index)" class="flex items-center cursor-pointer mt-4">
-            <component :is="errorMessage.expanded? ChevronDownIcon : ChevronRightIcon" class="h-5 w-5 text-red-500" aria-hidden="true" />
+            <component
+              :is="errorMessage.expanded ? ChevronDownIcon : ChevronRightIcon"
+              class="h-5 w-5 text-red-500"
+              aria-hidden="true"
+            />
             <span class="ml-2 text-red-700">Pipeline: {{ errorMessage.pipeline }}</span>
           </div>
           <div v-if="errorMessage.expanded" class="ml-5 mt-2">
             <div v-for="(issue, issueIndex) in errorMessage.issues" :key="issueIndex" class="mb-2">
-              <h4 v-if="issue.asset" class="text-md font-semibold text-gray-900">Asset: {{ issue.asset }}</h4>
+              <h4 v-if="issue.asset" class="text-md font-semibold text-gray-900">
+                Asset: {{ issue.asset }}
+              </h4>
               <p class="text-sm text-red-600">{{ issue.description }}</p>
               <div v-if="issue.context.length" class="flex items-center space-x-1 mt-2 justify-end">
-                <button @click="toggleIssueExpansion(index, issueIndex)" class="text-xs text-red-700 flex items-center">
+                <button
+                  @click="toggleIssueExpansion(index, issueIndex)"
+                  class="text-xs text-red-700 flex items-center"
+                >
                   <span class="mr-1">Details</span>
-                  <component :is="issue.expanded? ChevronUpIcon : ChevronDownIcon" class="h-4 w-4" aria-hidden="true" />
+                  <component
+                    :is="issue.expanded ? ChevronUpIcon : ChevronDownIcon"
+                    class="h-4 w-4"
+                    aria-hidden="true"
+                  />
                 </button>
               </div>
               <div v-if="issue.expanded" class="text-sm text-gray-700 ml-1 mt-1 w-full">
-                <pre class="whitespace-pre-wrap bg-red-100 rounded p-2">{{ issue.context.join("\n") }}</pre>
+                <pre class="whitespace-pre-wrap bg-red-100 rounded p-2">{{
+                  issue.context.join("\n")
+                }}</pre>
               </div>
             </div>
           </div>
@@ -79,7 +111,12 @@ import {
   ChevronUpIcon,
   XMarkIcon,
 } from "@heroicons/vue/20/solid";
-import type { FormattedErrorMessage, ParsedValidationErrorMessage, Issue, FormattedIssue } from "@/types";
+import type {
+  FormattedErrorMessage,
+  ParsedValidationErrorMessage,
+  Issue,
+  FormattedIssue,
+} from "@/types";
 
 // Define the props for the component
 const props = defineProps<{
@@ -98,106 +135,106 @@ const processedErrors = ref<FormattedErrorMessage[]>([]); // Holds the formatted
 const parseErrorMessage = (): FormattedErrorMessage[] => {
   if (!props.errorMessage) return []; // Return empty if no error message
 
-    try {
-      const errorObject = JSON.parse(props.errorMessage); // Parse the error message
-      if (errorObject.error) {
-        return createErrorMessage("Unknown", errorObject.error); // Handle unknown errors
-      }
-      if (Array.isArray(errorObject)) {
-        // Process validation errors
-        const formattedErrors = errorObject
-         .map((validationError: ParsedValidationErrorMessage) => ({
-            pipeline: validationError.pipeline || null,
-            expanded: false, // Set initially to false, will be updated after processing
-            issues: extractCriticalIssues(validationError.issues), // Extract critical issues
-          }))
-         .filter((error) => error.issues.length > 0); // Filter out errors with no issues
-
-        // Set expansion state based on number of pipelines
-        const isOneError = formattedErrors.length === 1;
-        return formattedErrors.map((error) => ({
-         ...error,
-          expanded: isOneError, // Expand if there's only one pipeline
-          issues: error.issues.map((issue) => ({
-           ...issue,
-            expanded: isOneError && isSingleAsset.value, // Expand issues for single asset/pipeline
-          })),
-        }));
-      }
-      return [];
-    } catch (e) {
-      console.error("Failed to parse error message:", e); // Log parsing errors
-      return createErrorMessage("Error", String(props.errorMessage)); // Handle parsing errors
+  try {
+    const errorObject = JSON.parse(props.errorMessage); // Parse the error message
+    if (errorObject.error) {
+      return createErrorMessage("Unknown", errorObject.error); // Handle unknown errors
     }
-  };
+    if (Array.isArray(errorObject)) {
+      // Process validation errors
+      const formattedErrors = errorObject
+        .map((validationError: ParsedValidationErrorMessage) => ({
+          pipeline: validationError.pipeline || null,
+          expanded: false, // Set initially to false, will be updated after processing
+          issues: extractCriticalIssues(validationError.issues), // Extract critical issues
+        }))
+        .filter((error) => error.issues.length > 0); // Filter out errors with no issues
 
-  // Helper function to create a formatted error message
-  const createErrorMessage = (pipeline: string, description: string) => [
-    {
-      pipeline,
-      expanded: true,
-      issues: [
-        {
-          asset: null,
-          description,
-          context: [],
-          expanded: true,
-          severity: "critical",
-        },
-      ],
-    },
-  ];
+      // Set expansion state based on number of pipelines
+      const isOneError = formattedErrors.length === 1;
+      return formattedErrors.map((error) => ({
+        ...error,
+        expanded: isOneError, // Expand if there's only one pipeline
+        issues: error.issues.map((issue) => ({
+          ...issue,
+          expanded: isOneError && isSingleAsset.value, // Expand issues for single asset/pipeline
+        })),
+      }));
+    }
+    return [];
+  } catch (e) {
+    console.error("Failed to parse error message:", e); // Log parsing errors
+    return createErrorMessage("Error", String(props.errorMessage)); // Handle parsing errors
+  }
+};
 
-  // Helper function to extract critical issues from validation errors
-  const extractCriticalIssues = (issues: any) => {
-    return Object.entries(issues || {}).flatMap(([test, issueList]) =>
-      (issueList as Issue[])
-       .filter((issue: Issue) => issue.severity === "critical")
-       .map(
-          (issue: Issue): FormattedIssue => ({
-            asset: issue.asset || null,
-            description: issue.description,
-            context: issue.context || [],
-            expanded: false, // Will be updated after initial processing
-            severity: issue.severity,
-          })
-        )
-    );
-  };
+// Helper function to create a formatted error message
+const createErrorMessage = (pipeline: string, description: string) => [
+  {
+    pipeline,
+    expanded: true,
+    issues: [
+      {
+        asset: null,
+        description,
+        context: [],
+        expanded: true,
+        severity: "critical",
+      },
+    ],
+  },
+];
 
-    // Computed properties
-    const isSingleAsset = computed(() => {
-    if (props.errorPhase!== "Validation") return false;
-    const firstError = processedErrors.value[0];
-    if (!firstError) return false;
-
-    const assets = new Set(firstError.issues.map((issue) => issue.asset));
-    return assets.size === 1;
-  });
-
-  const singleAssetName = computed(() => {
-    if (!isSingleAsset.value) return "";
-    return processedErrors.value[0].issues[0].asset || "Unknown Asset";
-  });
-  // Watch for changes in the errorMessage prop and reprocess the errors
-  watch(
-    () => props.errorMessage,
-    () => {
-      processedErrors.value = parseErrorMessage(); // Update processed errors on prop change
-    },
-    { immediate: true }
+// Helper function to extract critical issues from validation errors
+const extractCriticalIssues = (issues: any) => {
+  return Object.entries(issues || {}).flatMap(([test, issueList]) =>
+    (issueList as Issue[])
+      .filter((issue: Issue) => issue.severity === "critical")
+      .map(
+        (issue: Issue): FormattedIssue => ({
+          asset: issue.asset || null,
+          description: issue.description,
+          context: issue.context || [],
+          expanded: false, // Will be updated after initial processing
+          severity: issue.severity,
+        })
+      )
   );
+};
 
-  // Function to toggle the expansion state of an error message
-  const toggleExpansion = (index: number) => {
-    processedErrors.value[index].expanded =!processedErrors.value[index].expanded; // Toggle expansion
-  };
+// Computed properties
+const isSingleAsset = computed(() => {
+  if (props.errorPhase !== "Validation") return false;
+  const firstError = processedErrors.value[0];
+  if (!firstError) return false;
 
-  // Function to toggle the expansion state of an issue within an error message
-  const toggleIssueExpansion = (pipelineIndex: number, issueIndex: number) => {
-    processedErrors.value[pipelineIndex].issues[issueIndex].expanded =
-     !processedErrors.value[pipelineIndex].issues[issueIndex].expanded; // Toggle issue expansion
-  };
+  const assets = new Set(firstError.issues.map((issue) => issue.asset));
+  return assets.size === 1;
+});
+
+const singleAssetName = computed(() => {
+  if (!isSingleAsset.value) return "";
+  return processedErrors.value[0].issues[0].asset || "Unknown Asset";
+});
+// Watch for changes in the errorMessage prop and reprocess the errors
+watch(
+  () => props.errorMessage,
+  () => {
+    processedErrors.value = parseErrorMessage(); // Update processed errors on prop change
+  },
+  { immediate: true }
+);
+
+// Function to toggle the expansion state of an error message
+const toggleExpansion = (index: number) => {
+  processedErrors.value[index].expanded = !processedErrors.value[index].expanded; // Toggle expansion
+};
+
+// Function to toggle the expansion state of an issue within an error message
+const toggleIssueExpansion = (pipelineIndex: number, issueIndex: number) => {
+  processedErrors.value[pipelineIndex].issues[issueIndex].expanded =
+    !processedErrors.value[pipelineIndex].issues[issueIndex].expanded; // Toggle issue expansion
+};
 </script>
 
 <style scoped>
