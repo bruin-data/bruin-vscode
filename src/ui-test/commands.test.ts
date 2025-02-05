1fab7bd9183175d54846d2064430b95dd227e002
Djamila Baroudi <baroudi.d7@gmail.com>
2025-02-05 17:23:47 +0100

    update Windows executable path for version command in tests

--- src/ui-test/commands.test.ts
+++ src/ui-test/commands.test.ts
@@ -21,7 +21,8 @@ describe("Sample Command palette tests", function () {
 
     // Use Git Bash on Windows
     if (process.platform === "win32") {
-      await terminalView.executeCommand(`"${bruinExecutable}" --version`);
+      const bruinExecutableWindows = path.join(os.homedir(), ".local", "bin", "bruin.exe");
+      await terminalView.executeCommand(`"${bruinExecutableWindows}" --version`);
     } else {
       await terminalView.executeCommand(`${bruinExecutable} --version`);
     }
@@ -33,16 +34,20 @@ describe("Sample Command palette tests", function () {
     assert.strictEqual(versionAvailble, true);
   });
 
-  //test run command with dummy input and delay 
-  it("Testing run command with dummy call and delay", async () => {
+  //test run command with dummy input and delay
+  it("Testing run command with dummy call and delay", async function () {
+    let bruinExecutable = 'bruin';
+    if(process.platform === "win32"){
+       bruinExecutable = path.join(os.homedir(), ".local", "bin", "bruin.exe");
+    }
     const terminalView = await new TerminalView();
     await terminalView.selectChannel("Bruin Terminal");
     await terminalView.executeCommand(" ");
     await sleep(1000);
-    await terminalView.executeCommand("bruin run --help");
+    await terminalView.executeCommand(`"${bruinExecutable}" run --help`);
     const terminalOutput = await terminalView.getText();
     console.log("terminal output", terminalOutput);
-    const helpAvailable = terminalOutput.includes("bruin run - run a Bruin pipeline");        
+    const helpAvailable = terminalOutput.includes("bruin run - run a Bruin pipeline");
     assert.strictEqual(helpAvailable, true);
   });
 });
