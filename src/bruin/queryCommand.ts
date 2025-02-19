a4720bce2c0efec78267468071cb7fdb092a5a43
Djamila Baroudi <baroudi.d7@gmail.com>
2025-02-19 11:26:38 +0100

    remove escaping logic

--- src/bruin/queryCommand.ts
+++ src/bruin/queryCommand.ts
@@ -31,7 +31,7 @@ export class BruinQueryOutput extends BruinCommand {
         "-c", 
         connection, 
         "-q", 
-        query.replace(/'/g, "\\'"), // escape single quotes
+        query,
         "-o", 
         "json"
       ],
