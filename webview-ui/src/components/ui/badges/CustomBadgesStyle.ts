33e5a6823a2c10322f04ea20446c091566686eb3
Saravjeet 'Aman' Singh <turtleDev@users.noreply.github.com>
2025-06-17 22:46:52 +0530

    Add full seed asset type support

--- webview-ui/src/components/ui/badges/CustomBadgesStyle.ts
+++ webview-ui/src/components/ui/badges/CustomBadgesStyle.ts
@@ -25,6 +25,42 @@ export const badgeStyles = {
     'ingestr': {
         main: 'bg-amber-400/10 text-amber-400 ring-amber-400/30',
     },
+    'duckdb.seed': {
+        main: 'bg-rose-400/10 text-rose-400 ring-rose-400/30',
+    },
+    'emr_serverless.spark': {
+        main: 'bg-lime-400/10 text-lime-400 ring-lime-400/30',
+    },
+    'emr_serverless.pyspark': {
+        main: 'bg-lime-400/10 text-lime-400 ring-lime-400/30',
+    },
+    'bq.seed': {
+        main: 'bg-blue-400/10 text-blue-400 ring-blue-400/30',
+    },
+    'athena.seed': {
+        main: 'bg-teal-400/10 text-teal-400 ring-teal-400/30',
+    },
+    'clickhouse.seed': {
+        main: 'bg-pink-400/10 text-pink-400 ring-pink-400/30',
+    },
+    'databricks.seed': {
+        main: 'bg-red-400/10 text-red-400 ring-red-400/30',
+    },
+    'ms.seed': {
+        main: 'bg-violet-400/10 text-violet-400 ring-violet-400/30',
+    },
+    'pg.seed': {
+        main: 'bg-cyan-400/10 text-cyan-400 ring-cyan-400/30',
+    },
+    'rs.seed': {
+        main: 'bg-fuchsia-400/10 text-fuchsia-400 ring-fuchsia-400/30',
+    },
+    'sf.seed': {
+        main: 'bg-orange-400/10 text-orange-400 ring-orange-400/30',
+    },
+    'synapse.seed': {
+        main: 'bg-purple-400/10 text-purple-400 ring-purple-400/30',
+    },
     "python": {
         main: "bg-green-400/10 text-green-400 ring-green-400/30",
     },
