206c96bb9ed5118d471c5d82df4aaabb126b210b
Djamila Baroudi <baroudi.d7@gmail.com>
2025-07-01 11:19:19 +0200

    add focus command for QueryPreviewView in activation process

--- src/extension/extension.ts
+++ src/extension/extension.ts
@@ -260,6 +260,7 @@ export async function activate(context: ExtensionContext) {
           message: true,
           tabId: activeTabId,
         });
+        await commands.executeCommand('bruin.QueryPreviewView.focus');
         await getQueryOutput("", "", uri, activeTabId);
       } catch (error) {
         const errorMessage = error instanceof Error ? error.message : String(error);
