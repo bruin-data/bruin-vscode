0ee5f0c3558f63802fc1f2226c6a989c7c61a0c4
Mustafa <mstfa.ersan@hotmail.com>
2025-07-09 15:15:01 +0300

    add style to fill from db button

--- webview-ui/src/components/asset/columns/AssetColumns.vue
+++ webview-ui/src/components/asset/columns/AssetColumns.vue
@@ -3,18 +3,32 @@
     <div class="flex justify-end mb-4 space-x-2">
       <vscode-button @click="fillColumnsFromDB" :disabled="fillColumnsStatus === 'loading'">
         <template v-if="fillColumnsStatus === 'loading'">
-          <svg class="animate-spin mr-1 h-4 w-4 text-editor-bg" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+          <svg
+            class="animate-spin mr-1 h-4 w-4 text-editor-bg"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <circle
+              class="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              stroke-width="4"
+            ></circle>
+            <path
+              class="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            ></path>
           </svg>
         </template>
         <template v-else-if="fillColumnsStatus === 'success'">
           <CheckCircleIcon class="h-4 w-4 mr-1 text-editor-button-fg" aria-hidden="true" />
         </template>
         <template v-else-if="fillColumnsStatus === 'error'">
-          <svg class="h-4 w-4 mr-1 text-red-500" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
-          </svg>
+          <XCircleIcon class="h-4 w-4 mr-1 text-editorError-foreground" aria-hidden="true" />
         </template>
         Fill from DB
       </vscode-button>
@@ -288,6 +302,7 @@ import {
   LinkIcon,
   KeyIcon,
 } from "@heroicons/vue/20/solid";
+import { CheckCircleIcon, XCircleIcon } from "@heroicons/vue/24/solid";
 import DeleteAlert from "@/components/ui/alerts/AlertWithActions.vue";
 import { vscode } from "@/utilities/vscode";
 import { v4 as uuidv4 } from "uuid"; // Import UUID library to generate unique IDs
