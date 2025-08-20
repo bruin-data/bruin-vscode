cd1bd9ef5879219145600418ffa1b02e74cc9e04
Djamila Baroudi <baroudi.d7@gmail.com>
2025-08-20 18:10:39 +0200

    refactor getEnvListCommand to improve workspace directory handling and ensure fallback to the last rendered document's directory

--- src/extension/commands/getEnvListCommand.ts
+++ src/extension/commands/getEnvListCommand.ts
@@ -1,14 +1,20 @@
 import { BruinEnvList } from "../../bruin/bruinSelectEnv";
-import { Uri } from "vscode";
+import * as vscode from "vscode";
+import * as path from "path";
 import { bruinWorkspaceDirectory } from "../../bruin";
 import { getBruinExecutablePath } from "../../providers/BruinExecutableService";
 
-export const getEnvListCommand = async (lastRenderedDocumentUri:  Uri | undefined ) => {
+export const getEnvListCommand = async (lastRenderedDocumentUri: vscode.Uri | undefined) => {
+  if (!lastRenderedDocumentUri) {
+    return;
+  }
 
-  const getList = new BruinEnvList(
-    getBruinExecutablePath(),
-    await bruinWorkspaceDirectory(lastRenderedDocumentUri!.fsPath)!! as string
-  );
+  const foundBruinWorkspace = await bruinWorkspaceDirectory(lastRenderedDocumentUri.fsPath);
+  const workspaceFolder = vscode.workspace.getWorkspaceFolder(lastRenderedDocumentUri)?.uri.fsPath;
+  const fallbackDir = workspaceFolder ?? path.dirname(lastRenderedDocumentUri.fsPath);
+  const workingDirectory = foundBruinWorkspace ?? fallbackDir;
+
+  const getList = new BruinEnvList(getBruinExecutablePath(), workingDirectory as string);
   await getList.getEnvironmentsList();
-  };
+};
   
